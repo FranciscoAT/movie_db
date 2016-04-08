317c1aadf73df5145038cfd1e325a22079afc308
Francisco <francisco.ad.trindade@gmail.com>
2016-04-08 18:00:40 -0400

    Added OMDB script

--- webapp/public/scripts/omdbPull.js
+++ webapp/public/scripts/omdbPull.js
@@ -1,7 +1,22 @@
-var http = required('http');
+var http = require('http');
+var fs = require('fs');
 
 function getMovie(Name){
-    
+   var options = {
+       host: 'www.omdbapi.com',
+       port: 80,
+       path: '/?/?plot=full&r=json&t='+Name
+    };
 
-}
-
+    http.get(options, function(res){
+        var data = '';
+        res.on('data', function(chunk){
+            data += chunk;
+        });
+        res.on('end', function(){
+            fs.writeFile('tempMovie.json', data);
+        });
+    }).on('error', function(e) {
+        console.log("Got error: "+e.message);
+    });
+};
